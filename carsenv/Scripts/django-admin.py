af0b60e6c4f12d941cff0ae1118f67a20b0744fd
Priyanka <priyanka19697@gmail.com>
2022-05-25 23:40:47 +0530

    initial commit

---
+++ carsenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\ASUS\OneDrive\Desktop\Progress\Project1 - Carzone\carsenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
